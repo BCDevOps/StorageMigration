c896eb019f059fddebc90a4148daf3da286749a5
Wade Barnes <wade.barnes@shaw.ca>
2019-02-21 13:29:23 -0800

    Add manage script
    - Automates the PVC migration process.

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,4 +1,2 @@
-# You WILL need to override the namespace using a settings.local.sh file
-export PROJECT_NAMESPACE="bcgov"
-
-export GIT_URI="https://github.com/BCDevOps/StorageMigration.git"
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-"bcgov"}
+export GIT_URI=${GIT_URI:-"https://github.com/BCDevOps/StorageMigration.git"}

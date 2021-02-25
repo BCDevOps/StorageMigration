afed7783db19f15050fc46b973af9d6f1820d5df
wadeking98 <wkingnumber2@gmail.com>
2021-02-25 10:24:41 -0800

    temporarily added overrides into settigns
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- cross-cluster/openshift/settings.source.sh
+++ cross-cluster/openshift/settings.source.sh
@@ -1,2 +1,7 @@
 export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-"devex-von"}
 export include_templates="source-pvc-deploy"
+
+export REMOTE_CLUSTER=$(echo ${DST_CONTEXT} | awk 'BEGIN { FS = "/" } ; { print $2 }' )
+export REMOTE_PROJECT=$DST_CLUSTER
+export DST_CLUSTER=$(echo ${DST_CONTEXT} | awk 'BEGIN { FS = "/" } ; { print $1 }' )
+export SRC_CLUSTER=$(echo ${SRC_CONTEXT} | awk 'BEGIN { FS = "/" } ; { print $1 }' )
--- cross-cluster/openshift/settings.target.sh
+++ cross-cluster/openshift/settings.target.sh
@@ -1,2 +1,5 @@
 export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-"4a9599"}
 export include_templates="target-pvc-deploy"
+
+export DST_CLUSTER=$(echo ${DST_CONTEXT} | awk 'BEGIN { FS = "/" } ; { print $1 }' )
+export SRC_CLUSTER=$(echo ${SRC_CONTEXT} | awk 'BEGIN { FS = "/" } ; { print $1 }' )
